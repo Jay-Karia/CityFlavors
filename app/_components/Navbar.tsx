1fa8015359d931f0ddc7d0c361d9a93bcfe9b700
Jay <jay.sanjay.karia@gmail.com>
2024-02-21 12:41:24 +0530

    Add files via upload

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,17 +1,23 @@
 import Logo from "@/components/Logo"
 
 import { Button } from "@/components/ui/button"
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
 
+import Image from "next/image"
 import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div className="flex min-h-14 items-center justify-around bg-transparent">
-
+    <div className="flex min-h-14 items-center sm:justify-around justify-between mx-2 bg-transparent">
       <Logo />
-
-      <div className="flex gap-5">
-
+      <div className="hidden gap-5 sm:block">
         <Link href="/">
           <Button variant={"link"}>Home</Button>
         </Link>
@@ -27,19 +33,38 @@ const Navbar = () => {
         <Link href="/contact">
           <Button variant={"link"}>Contact Us</Button>
         </Link>
-
       </div>
 
-      <div className="flex space-x-5">
-
+      <div className="hidden sm:block space-x-5">
         <Link href="/login">
           <Button variant={"link"}>Login</Button>
         </Link>
         <Link href="/register">
           <Button variant={"outline"}>Register</Button>
         </Link>
+      </div>
+
+      {/* Small Screen Navbar */}
+      <div className="block sm:hidden">
+        <Menubar>
+          <MenubarMenu>
+            <MenubarTrigger>
+              <Image src="/menu.svg" alt="menu" width={20} height={20} />
+            </MenubarTrigger>
+            <MenubarContent >
+              <Link href="/"><MenubarItem className="hover:bg-slate-200">Home</MenubarItem></Link>
+              <Link href="/items"><MenubarItem className="hover:bg-slate-200">Items</MenubarItem></Link>
+              <Link href="/about"><MenubarItem className="hover:bg-slate-200">About Us</MenubarItem></Link>
+              <Link href="/contact"><MenubarItem className="hover:bg-slate-200">Contact Us</MenubarItem></Link>
+              <MenubarSeparator className="bg-slate-300" />
+              <Link href="/login"><MenubarItem className="hover:bg-slate-200">Login</MenubarItem></Link>
+              <Link href="/register"><MenubarItem className="hover:bg-slate-200">Register</MenubarItem></Link>
+            </MenubarContent>
+          </MenubarMenu>
+        </Menubar>
 
       </div>
+
     </div>
   )
 }
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,9 +4,9 @@ import Link from "next/link"
 const Logo = () => {
   return (
     <Link href="/">
-      <div className="flex space-x-5 items-center">
-        <Image src="/building.png" alt="logo" width={30} height={5} />
-        <h3 className="scroll-m-20 text-2xl font-normal tracking-tight">
+      <div className="flex sm:space-x-5 space-x-2 items-center">
+        <Image src="/building.png" alt="logo" width={25} height={5} className="sm:w-7" />
+        <h3 className="scroll-m-20 sm:text-2xl font-normal tracking-tight text-xl">
           CityFlavors
         </h3>
       </div>
